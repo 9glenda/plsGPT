8574fac9331c8f1e8300a7d13175ae1de74c8990
9glenda <plan9git@proton.me>
2023-02-12 12:00:44 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "A simple Go package";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "github:Nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          plsgpt = pkgs.buildGoModule {
+            pname = "plsgpt";
+            inherit version;
+            # In 'nix develop', we don't need a copy of the source tree
+            # in the Nix store.
+            src = ./.;
+
+            # vendorSha256 = pkgs.lib.fakeSha256;
+            vendorSha256 = "sha256-qCe0WBt99TggAtTlMpuHoRkiocNnBK+sAz3WnBHHE5M=";
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.plsgpt}/bin/plsgpt";
+        };
+      });
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+
+      devShells = forAllSystems (system: {
+        default = nixpkgsFor.${system}.mkShell {
+          packages = [
+            nixpkgsFor.${system}.go
+            self.packages.${system}.plsgpt
+          ];
+        };
+      });
+
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.plsgpt);
+    };
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/9glenda/plsgpt
+
+go 1.19
+
+require github.com/PullRequestInc/go-gpt3 v1.1.11
